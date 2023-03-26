26aa5b2e419aaed66d5e2f22de22f7799288d047
grine2003 <loordmohammed344@gmail.com>
2023-03-26 18:00:07 +0200

    fonts kan dakhel assets, raja3to bara

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'package:appcouvoiturage/pages/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:appcouvoiturage/pages/signup.dart';
 import 'package:appcouvoiturage/pages/login.dart';
+import 'package:appcouvoiturage/pages/trajetdetails.dart';
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(fontFamily: 'Poppins'),
-    home: Wrapper(),
+    home: Details(
+        carName: "Mersedes",
+        email: "lh_boulacheb@esi.dz",
+        fullName: "Hichem Boulacheb",
+        phoneNumber: "0321659874",
+        photoUrl: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
+        rating: 3.5,),
   ));
 }
 class MyApp extends StatelessWidget {
@@ -140,8 +147,6 @@ class _MyConnexinState extends State<Connexin> {
             ]
         ),
       ),
-      //bottomNavigationBar: BottomAppBar(),
-
     );
 
 
