640f3c94936bd8edf537ea6a72284522939a86bf
yasseresi <ly_korzane@esi.dz>
2023-04-20 09:01:18 +0100

    Update Commancer.dart

--- lib/pages/Commancer.dart
+++ lib/pages/Commancer.dart
@@ -1,1 +1,100 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'hicham bourma',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Commancer(title: 'yes'),
+    );
+  }
+}
+
+class Commancer extends StatefulWidget {
+  const Commancer({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Commancer> createState() => _CommancerState();
+}
+
+class _CommancerState extends State<Commancer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Positioned.fill(
+          child: Image.asset(
+            'assets/images/welcome.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Welcome to my app',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet magna ut ex volutpat, sit amet tincidunt lorem malesuada. In vestibulum quis odio at finibus. ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Log in'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Sign up'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ));
+  }
+}
